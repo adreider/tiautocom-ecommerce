2726bc848a55ede044d7b5514617d5843a5e5c9c
adreider <adreidereconomic@gmail.com>
2021-09-06 17:15:11 -0300

    add FKdepartment_id_product

--- src/database/migrations/1630957850216-CreateFKdepartment_product.ts
+++ src/database/migrations/1630957850216-CreateFKdepartment_product.ts
@@ -1,11 +1,23 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, TableForeignKey } from "typeorm";
 
 export class CreateFKdepartmentProduct1630957850216 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createForeignKey(
+            "product",
+            new TableForeignKey({
+                name: 'FKdepartment_product',
+                referencedTableName: "department",
+                referencedColumnNames: ["id"],
+                columnNames: ["department_id"],
+                onDelete: "SET NULL",
+                onUpdate: "SET NULL",
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("product", "FKdepartment_product")
     }
 
 }
